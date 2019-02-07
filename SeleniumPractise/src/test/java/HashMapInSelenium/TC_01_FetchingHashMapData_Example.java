6ea728f1853304bb0ebeb02192a68bbb77e61868
hiteshg <hiteshg@PG01CBAC.pyramidconsultinginc.com>
2019-02-07 19:12:14 +0530

    c

--- SeleniumPractise/src/test/java/HashMapInSelenium/TC_01_FetchingHashMapData_Example.java
+++ SeleniumPractise/src/test/java/HashMapInSelenium/TC_01_FetchingHashMapData_Example.java
@@ -1,13 +1,10 @@
 package HashMapInSelenium;
 
-import java.util.HashMap;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_01_FetchingHashMapData_Example {
